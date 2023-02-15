ec53f3e6d490d65c93739c7480d145b1d4dfcddc
Github Sync Pipeline <axytos-ecommerce-plugins@brickmakers.de>
2023-02-15 16:49:05 +0000

    Publish Version 1.4.0, Build 20230215.5

--- tests/Integration/OrderSync/ReportUpdateTest.php
+++ tests/Integration/OrderSync/ReportUpdateTest.php
@@ -78,4 +78,41 @@ class ReportUpdateTest extends OrderSyncWorkerIntegratedTestCase
             ]
         ]]);
     }
+
+
+
+    /**
+     * @return void
+     */
+    public function test_does_not_report_update_for_orders_without_basket_updates()
+    {
+        $this->executeTestCases([[
+            'order' => [
+
+                'hasCreateInvoiceReported' => false,
+                'hasBeenInvoiced' => false,
+
+                'hasCancelReported' => false,
+                'hasBeenCanceled' => false,
+
+                'hasRefundReported' => false,
+                'hasBeenRefunded' => false,
+
+                'hasShippingReported' => false,
+                'hasBeenShipped' => false,
+
+                'hasNewTrackingInformation' => false,
+
+                'hasBasketUpdates' => false,
+            ],
+            'expected' => [
+                'reportCancel' => false,
+                'reportCreateInvoice' => false,
+                'reportRefund' => false,
+                'reportShipping' => false,
+                'reportTrackingInformation' => false,
+                'reportUpdate' => false,
+            ]
+        ]]);
+    }
 }
